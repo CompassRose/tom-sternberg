5bcc12a63d479f33c9dc53c36f1e3bab1edc5870
Tom Sternberg <tomc-s@comcast.net>
2018-10-22 13:28:10 -0700

    Fixed landing element

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -42,9 +42,9 @@ export class HomeComponent implements OnInit {
       if (this.identity) {
         this.loadAllUsers();
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        //  this.router.navigate(['app/wheelspin']);
+        this.router.navigate(['app/wheelspin']);
         // this.router.navigate(['app/family/family-routes/worldmap-locator']);
-        this.router.navigate(['app/family/family-routes/dTree']);
+        // this.router.navigate(['app/family/family-routes/dTree']);
         // this.router.navigate(['app/family/family-routes/pictures']);
       } else {
         this.router.navigate(['app/login']);
